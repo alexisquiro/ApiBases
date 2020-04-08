6064a00f9a5fcdcc0d3d6cdf146832e6fce173ba
alexis <sixela.quiros@gmail.com>
2020-04-08 11:27:36 -0400

    api login primer commit

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const express=require('express');
+const app=express();
+
+
+//setting
+
+app.set('port',process.env.PORT||4000);
+
+
+
+//middlewares
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(function(req, res, next) {
+
+    res.header("Access-Control-Allow-Origin", "*");
+  
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  
+    next();
+  
+  });
+
+
+
+//routes 
+
+app.use(require('./routes /user/index'));
+
+
+//start server
+app.listen(app.get('port'));
